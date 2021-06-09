e55aa07126f976d8db2848906a26f1cd47d6e1fb
ChillarAnand <anand21nanda@gmail.com>
2021-06-09 08:27:15 +0530

    chore: Bump version to 0.1.5

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ import setuptools
 
 setuptools.setup(
     name="stocktrends",
-    version="0.1.4",
+    version="0.1.5",
     url="https://github.com/chillaranand/stocktrends",
 
     author="chillar anand",
     author_email="chillar@avilpage.com",
 
-    description="Python package to plot stock trends with charts like renko, line break, pnf etc",
+    description="Python package to plot stock, derivative(futures & options) trends with charts like renko, line break, pnf etc",
     long_description=open('README.rst').read(),
 
     packages=setuptools.find_packages(),
@@ -17,12 +17,16 @@ setuptools.setup(
     install_requires=[],
 
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
+        'Development Status :: 5 - Production/Stable',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
 )
