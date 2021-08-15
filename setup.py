b1272ca8f96ee01b411b37eb3f830a2dd6810a8a
ChillarAnand <anand21nanda@gmail.com>
2021-08-15 11:36:05 +0530

    chore: Code cleanup

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 import setuptools
 
-
 setuptools.setup(
     name="stocktrends",
     version="0.1.5",
@@ -17,7 +16,7 @@ setuptools.setup(
     install_requires=[],
 
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
+        'Development Status :: 5 - Production/Stable'
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
